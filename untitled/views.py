27b6714ee51b172d27fb965e878a091d4fc580cf
Korsunow Konstantin <korsunowk@gmail.com>
2017-07-01 08:47:47 +0300

    Update views.py

--- untitled/views.py
+++ untitled/views.py
@@ -546,7 +546,7 @@ def create_otchet(selss, variety):
         c.drawString(50, y, str(index))
         c.drawString(100, y, i.seans_id.film.name)
         c.drawString(160, y - 25, 'Дата сеанса:')
-        c.drawString(160, y - 50, 'Время сеанса :')
+        c.drawString(160, y - 50, 'Время сеанса:')
         c.drawString(160, y - 75, 'Количество проданных билетов : ')
         c.drawString(160, y - 100, 'Общая выручка : ')
         c.drawString(465, y - 25, str(i.seans_id.date))
