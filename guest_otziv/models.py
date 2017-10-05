f3803378023ed30800a77e5a05a9ded65a9d4e19
Korsunow Konstantin <korsunowk@gmail.com>
2017-10-05 14:40:57 +0300

    Update models.py

--- guest_otziv/models.py
+++ guest_otziv/models.py
@@ -7,9 +7,6 @@ class GuestOtziv(models.Model):
     text = models.TextField(max_length=20000)
     date = models.DateField()
 
-    def __unicode__(self):
-        return self.name
-
     def __str__(self):
         return self.name
 
