792e2c490c01900e9484c7210006d49ce918047c
Korsunow Konstantin <korsunowk@gmail.com>
2017-10-24 19:57:19 +0300

    Update admin.py

--- kinouser/admin.py
+++ kinouser/admin.py
@@ -16,7 +16,6 @@ class UserCreationForm(forms.ModelForm):
         fields = ('email',)
 
     def clean_password2(self):
-
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
